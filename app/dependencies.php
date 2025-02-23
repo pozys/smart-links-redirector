1036104e93b77adc81e0cc43b707ebde73549d41
Культяпов Константин <pozys@mail.ru>
2025-02-23 21:35:49 +0300

    add index file

---
+++ app/dependencies.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Application\Settings\SettingsInterface;
+use DI\ContainerBuilder;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor\UidProcessor;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+
+return function (ContainerBuilder $containerBuilder) {
+    $containerBuilder->addDefinitions([]);
+};
---
+++ public/index.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Application\ResponseEmitter\ResponseEmitter;
+use DI\ContainerBuilder;
+use Slim\Factory\AppFactory;
+use Slim\Factory\ServerRequestCreatorFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$containerBuilder = new ContainerBuilder();
+
+$settings = require __DIR__ . '/../app/settings.php';
+$settings($containerBuilder);
+
+$dependencies = require __DIR__ . '/../app/dependencies.php';
+$dependencies($containerBuilder);
+
+$repositories = require __DIR__ . '/../app/repositories.php';
+$repositories($containerBuilder);
+
+$container = $containerBuilder->build();
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+$callableResolver = $app->getCallableResolver();
+
+$serverRequestCreator = ServerRequestCreatorFactory::create();
+$request = $serverRequestCreator->createServerRequestFromGlobals();
+
+$responseFactory = $app->getResponseFactory();
+
+$app->addRoutingMiddleware();
+
+$app->addBodyParsingMiddleware();
+
+$response = $app->handle($request);
+$responseEmitter = new ResponseEmitter();
+$responseEmitter->emit($response);
